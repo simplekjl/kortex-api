cb6709f97a09ae89a30189cd4489d778e53f2873
Jose L Crisostomo <jl.cs606@gmail.com>
2018-04-25 15:13:14 -0700

    KortexAPI | WebSecurityLogic using Auth0

--- src/main/kotlin/com/kortex/api/ApiApplication.kt
+++ src/main/kotlin/com/kortex/api/ApiApplication.kt
@@ -3,6 +3,7 @@ package com.kortex.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class ApiApplication
 
